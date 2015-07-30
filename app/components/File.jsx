9f7996a82eb6ec311070698ace519a67cab06094
jim <jim@jimsaunders.net>
2015-07-30 18:58:27 -0400

    Moved individual comments into their own component. Removed all the self, this bullshit

--- app/components/File.jsx
+++ app/components/File.jsx
@@ -10,24 +10,23 @@ module.exports = React.createClass({
     },
 
     render: function() {
-        var self = this;
         var rows = [];
-        var lineCount = self.props.lines.length;
+        var lineCount = this.props.lines.length;
 
         for (var i=0; i < lineCount; i++) {
             var num = i + 1;
 
-            if (self.props.comments[i+1]) {
-                rows.push(<Line key={self.props.name + num} file={self.props.name} number={num} content={self.props.lines[i]} toggle={self.props.name+"-C"+(i + 1)}/>);
-                rows.push(<LineComments key={self.props.name + num + 'comments'} file={self.props.name} number={num} comments={self.props.comments[num]}/>);
+            if (this.props.comments[i+1]) {
+                rows.push(<Line key={this.props.name + num} file={this.props.name} number={num} content={this.props.lines[i]} toggle={this.props.name+"-C"+(i + 1)}/>);
+                rows.push(<LineComments key={this.props.name + num + 'comments'} file={this.props.name} number={num} comments={this.props.comments[num]}/>);
             } else {
-                rows.push(<Line key={self.props.name + num} file={self.props.name} number={num} content={self.props.lines[i]}/>);
+                rows.push(<Line key={this.props.name + num} file={this.props.name} number={num} content={this.props.lines[i]}/>);
             }
         }
 
         return (
             <section className="code-file-container">
-                <table id={self.props.name} className="code-file">
+                <table id={this.props.name} className="code-file">
                     <tbody>
                         <tr className="spacer line">
                             <td/>
@@ -58,13 +57,12 @@ var Line = React.createClass({
     },
 
     render: function() {
-        var self = this;
-        var toggleCol = self.props.toggle ? <td><CommentToggle toggle={self.props.toggle}/></td> : <td/>
+        var toggleCol = this.props.toggle ? <td><CommentToggle toggle={this.props.toggle}/></td> : <td/>
         return (
-            <tr id={self.props.file+"-L"+self.props.number} className="line">
+            <tr id={this.props.file+"-L"+this.props.number} className="line">
                 {toggleCol}
-                <td className="line-num">{self.props.number}</td>
-                <td className="line-content"><pre dangerouslySetInnerHTML={{__html: self.props.content}}/></td>
+                <td className="line-num">{this.props.number}</td>
+                <td className="line-content"><pre dangerouslySetInnerHTML={{__html: this.props.content}}/></td>
             </tr>
         );
     }
@@ -80,24 +78,15 @@ var LineComments = React.createClass({
     },
 
     render: function() {
-        var self = this;
         return (
-            <tr id={self.props.file+"-C"+self.props.number} className="line comment-row">
+            <tr id={this.props.file+"-C"+this.props.number} className="line comment-row">
                 <td/>
                 <td className="line-num"/>
                 <td>
                 {
-                    self.props.comments.map(function(comment) {
+                    this.props.comments.map(function(comment) {
                         return (
-                            <div key={comment.id} id={self.props.file+"-L"+self.props.number+"-C"+comment.id} className="line-comment">
-                                <a className="avatar pull-left" href={comment.user.html_url}><img src={comment.user.avatar_url} alt=""/></a>
-                                <div className="pull-left content">
-                                    <header className="comment-header">
-                                        <a href={comment.user.html_url}>{comment.user.login}</a>
-                                    </header>
-                                    <p className="comment-body">{comment.body}</p>
-                                </div>
-                            </div>
+                            <Comment key={comment.id} id={this.props.file+"-L"+this.props.number+"-C"+comment.id} user={comment.user} text={comment.body}/>
                         );
                     })
                 }
@@ -107,9 +96,35 @@ var LineComments = React.createClass({
     }
 });
 
+var Comment = React.createClass({
+    getDefaultProps: function() {
+        return {
+            id: '',
+            text: '',
+            user: null
+        };
+    },
+
+    render: function() {
+        return (
+            <div className="line-comment">
+                <a className="avatar pull-left" href={this.props.user.html_url}><img src={this.props.user.avatar_url} alt=""/></a>
+                <div className="pull-left content">
+                    <header className="comment-header">
+                        <a href={this.props.user.html_url}>{this.props.user.login}</a>
+                    </header>
+                    <p className="comment-body">{this.props.text}</p>
+                </div>
+            </div>
+        );
+    }
+});
+
 var CommentForm = React.createClass({
     getDefaultProps: function() {
         return {
+            text: '',
+            user: null,
             onSubmit: function() {},
             onCancel: function() {}
         }
@@ -121,7 +136,7 @@ var CommentForm = React.createClass({
                 <a className="avatar pull-left" href="#"><img src="" alt=""/></a>
                 <div className="pull-left content">
                     <form action="#" onSubmit={this.props.onSubmit} className="comment-body">
-                        <textarea name="comment-body" placeholder="Enter your comment..."></textarea>
+                        <textarea name="comment-body" placeholder="Enter your comment...">{this.props.text}</textarea>
                         <button type="submit" className="pure-button button-primary">
                             <i className="fa fa-comment"/> Comment
                         </button>
