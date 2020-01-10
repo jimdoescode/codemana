e4b221bc59ce9651f13ba0d1787ae7ec295fbddc
jimdoescode <jimdoescode@gmail.com>
2020-01-09 23:24:05 -0600

    Github is deprecating API access via user name and password. Now api access will only be available with a personal access token.

--- src/LoginModal.js
+++ src/LoginModal.js
@@ -20,7 +20,7 @@ class LoginModal extends Component {
             <form className="pure-form pure-form-stacked" onSubmit={e => this.props.onLogin(e.target.elements.namedItem("username").value.trim(), e.target.elements.namedItem("password").value)}>
                 <fieldset>
                     <input name="username" className="pure-input-1" type="text" placeholder="GitHub User Name..."/>
-                    <input name="password" className="pure-input-1" type="password" placeholder="GitHub Password or Token..."/>
+                    <input name="password" className="pure-input-1" type="password" placeholder="GitHub Access Token..."/>
                 </fieldset>
                 <fieldset className="buttons">
                     <button type="submit" className="pure-button button-primary"><i className="fa fa-save" aria-hidden="true"/> Save</button>
@@ -32,11 +32,7 @@ class LoginModal extends Component {
         return (
             <Modal contentLabel="Modal" isOpen={this.props.show} onRequestClose={this.props.onClose} className="react-modal-content" overlayClassName="react-modal-overlay">
                 <h2><i className="fa fa-github"/> GitHub Access</h2>
-                <p>To leave a comment you need to enter your GitHub user name and GitHub password. This is <strong>only</strong> used to post Gist comments to GitHub.</p>
-                <p>
-                    If you prefer not to enter your password you can use a <a target="_blank" rel="noopener noreferrer" href={this.props.github + "/settings/tokens/new?description=CodeMana&scopes=gist"}>personal access token</a>.
-                    <br/><i className="fa fa-asterisk" aria-hidden="true"/>Make sure it has Gist access.
-                </p>
+                <p>To leave a comment you need to enter your GitHub user name and GitHub <a target="_blank" rel="noopener noreferrer" href={this.props.github + "/settings/tokens/new?description=CodeMana&scopes=gist"}>personal access token</a>. This is <strong>only</strong> used to post Gist comments to GitHub.</p>
                 <hr/>
                 { this.props.processing ? <Spinner className="fa-github-alt"/> : form }
             </Modal>
